0f5099d8404b239df819efe626be999f9bee52dd
djalmafo <djalmafreire@outlook.com>
2022-10-21 17:48:07 -0300

    feat: :art: Correct file

--- src/print.test.js
+++ src/print.test.js
@@ -1,5 +1,5 @@
 const printWelcomes = require('./print');
 
 test("Print message 'Welcome João!'", () =>{
-    expect(printWelcomes("Joãoo")).toBe("Welcome João!");
+    expect(printWelcomes("João")).toBe("Welcome João!");
 } )
